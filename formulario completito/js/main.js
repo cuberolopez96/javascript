0e2fd029297d6b9b67dafb7b1d3429e36491b1d6
cuberolopez96 <cuberolopez96@hotmail.com>
2016-01-12 12:44:21 +0100

    formulario 2

--- formulario completito/js/main.js
+++ formulario completito/js/main.js
@@ -1,164 +1,67 @@
 (function(){
-	var dni,nombre,correo,edad,fnacimiento,urlblog,telefono,masculino,femenino;
+	var nombre,dni,correo,edad,fnacimiento,urlblog,telefono,genero;
 
-	function validarCampoVacio(text){
-		patron = RegExp(".+");
-		if(patron.test(text))
-			return true;
-		else
-			return false;
+	function comprobarCampoTexto(texto,patron){
+		var pattern = RegExp(patron);
+		return pattern.test(texto);
 	}
-	function validarCorreo(correo){
-		var patron = RegExp("^.+@.+\\..+$");
-		if(patron.test(correo))
-			return true;
-		else
-			return false;
-	}
-	function validarEdad(numero){
-		var patron = RegExp("\\d+");
-		if(patron.test(numero)){
-			return true;
-		}else{
-			return false;
-		}
-
-	}
-	function validarFecha(fecha){
-		var patron = RegExp("^\\d\\d/\\d\\d/\\d\\d\\d\\d$");
-		if(patron.test(fecha)){
-			fecha = fecha.split("/");
-			var dia = fecha[0];
-			var mes = fecha[1];
-			var anho = fecha[2];
-			var fecha = new Date();
-			var hoy  = new Data();
-			fecha.setDate(dia);
-			fecha.setMonth(mes - 1);
-			fecha.setFullYear(anho);
-			if(hoy.getTime() - fecha.getTime()< 0){
-				return false;
-			}else{
-				if(fecha.getDate()== dia && fecha.getMonth() == mes - 1&& fecha.getFullYear() == anho){
-					return true;
-				}else{
-					return false;
-				}
-			}
-
-
-		}else{
-			return false;
-		}
-	}
-	function validarDni(dni){
+	function comprobarDni(dni){
 		var cifras = dni.substr(0,8);
 		var letra = dni.substr(8,9);
 		var letras = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E", "T"];
-		var sumatorio = 0;
-		var patron = RegExp("^\\d{8}[A-Z]$");
-		if(patron.test(dni)){
-			for(i = 0; i<=9;i++){
-				sumatorio += cifras.charAt(i);
-			}
-			if(letra == letras[(sumatorio%23)]){
-				return true;
-			}else{
-				return false;
-			}
-		}else{
-			return false;
+		var contador = 0;
+		for(var i = 0;i<=7;i++){
+			contador += parseInt(dni.charAt(i));
+
 		}
-
-	}
-	function validarCuenta(iban){
-		var patron = RegExp("^ES\\d{22}$");
-		if(patron.test(iban))
-			return true;
-		else
+		if(letra != letras[parseInt(contador/23)])
 			return false;
-
+		return true;
 	}
-	function validarTelefono(telefono){
-		var patron = RegExp("^\\d{9}$");
-		if(patron.test(telefono))
-			return true;
-		else
+	function comprobarFecha(fecha){
+		fecha = fecha.split("/");
+		var dia = fecha[0];
+		var mes = fecha[1];
+		var anho = fecha[2];
+		fecha = new Date();
+		fecha.setDate(dia);
+		fecha.setMonth(mes - 1);
+		fecha.setFullYear(anho);
+		var hoy = new Date();
+		if(hoy.getTime() - fecha.getTime())
 			return false;
+		if(dia != fecha.getDate())
+			return false;
+		if(mes != fecha.getMonth() - 1)
+			return false;
+		if(anho != fecha.getFullYear())
+			return false;
+		return true;
 	}
 	window.addEventListener("load",function(){
-		var enviar = document.getElementById("enviar");
 		var error = document.getElementById("error");
-		 nombre =document.getElementById("nombre");
-		 dni = document.getElementById("dni");
-		 correo = document.getElementById("correo");
-		 edad = document.getElementById("edad");
-		 masculino = document.getElementById("masculino");
-		 femenino = document.getElementById("femenino");
+		var enviar = document.getElementById("enviar");
+		nombre = document.getElementById("nombre");
+		dni = document.getElementById("dni");
+		correo= document.getElementById("correo");
+		fnacimiento = document.getElementById("fnacimiento");
+		edad = document.getElementById("edad");
+		urlblog = document.getElementById("urlblog");
+		telefono = document.getElementById("telefono");
+		genero = document.getElementsByName("genero");
 
-		 fnacimiento = document.getElementById("fnacimiento");
-		 telefono = document.getElementById("telefono");
-		 nombre.addEventListener("blur",function(){
-			if(validarCampoVacio(nombre.value)== false){
+		nombre.addEventListener("blur",function(){
+			if(!comprobarCampoTexto(nombre.value,"^.+$")){
+				error.innerHTML = "nombre invalido";
 				nombre.className = "invalido";
-				error.innerHTML = "campo vacio";
-				nombre.focus();
-
 			}
 		});
 		dni.addEventListener("blur",function(){
-			if(validarDni(dni.value)== false){
+			if(!comprobarCampoTexto(dni.value,"^\\d{8}[A-Z]")||!comprobarDni(dni.value)){
+				error.innerHTML = "dni invalido";
 				dni.className = "invalido";
-				error.innerHTML = "dni invalido";
-				dni.focus();
-				
-			}
-		});
-
-		fnacimiento.addEventListener("blur",function(){
-			if(validarFecha(fnacimiento.value)== false){
-				fnacimiento.className = "invalido";
-				error.innerHTML = "dni invalido";
-				fnacimiento.focus();
-				
 			}
 		});
 		
-		correo.addEventListener("blur",function(){
-			if(validarCorreo(correo.value)== false){
-				correo.className = "invalido";
-				error.innerHTML = "correo invalido";
-				correo.focus();
-			}
-		});
-		edad.addEventListener("blur",function(){
-			if(validarEdad(edad.value)== false){
-				edad.className = "invalido";
-				error.innerHTML = "edad invalida";
-				edad.focus();
-			}
-		});
-		
-		telefono.addEventListener("blur",function(){
-			if(validarTelefono(telefono.value)== false){
-				telefono.className = "invalido";
-				error.innerHTML = "telefono invalido";
-				telefono.focus();
-			}
-		});
-		
-		
-		
-		
-		
-			
-
-
-
-		});
-
-
-	})();
-
-	
-
+	});
+})()
